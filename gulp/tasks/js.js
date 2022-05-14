1c1dbfdf3dbb2566f80e56f8b9b6f92ea592f4a7
ZianU <hunnybofficial@gmail.com>
2022-05-14 12:45:46 +0300

    Babel support added

--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -1,4 +1,5 @@
 import webpack from "webpack-stream";
+import babel from "gulp-babel";
 
 export const js = () => {
     return app.gulp.src(app.path.src.js, {sourcemaps: app.isDev})
@@ -8,6 +9,9 @@ export const js = () => {
             message: "Error: <%= error.message %>"
         })
     ))
+    .pipe(babel({
+        presets: ["@babel/preset-env"]
+    }))
     .pipe(webpack({
         mode: app.isBuild ? 'production' : 'development',
         output: {
